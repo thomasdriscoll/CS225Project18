4350a2530d31be9f253b5f4b7f2035bb95ead08d
Thomas <thomasdriscoll98@gmail.com>
2018-06-04 16:55:07 -0500

    adding numbers for nodes

--- world1.py
+++ world1.py
@@ -13,19 +13,50 @@
 # 3)
 
 #Calls the pygame library
-import pygame, random
+import pygame, random, os, sys
 #Optional: Sets up constants and functions in global namespace
 from pygame.locals import *
 #Global Color
 BLACK = (0, 0, 0)
+def load_image(name):
+    #Creates a full path for name to the data subdirectory
+    #Think linux: /home/user/directory/subdirectory/etc./data
+    fullname = os.path.join('numbers', name)
+    #try function attempts to load image to see if that works
+    try:
+        image = pygame.image.load(fullname)
+    #If try function fails, exits system
+    except pygame.error, message:
+        print 'Cannot load image:', name
+        raise SystemExit, message
+    #Makes a new copy of a Surface and converts color format/depth for display
+    image = image.convert()
+    #Sets colorkey for image
+    if colorkey is not None:
+    #If colorkey is -1, color of image is set to topleft pixel color
+        if colorkey is -1:
+            colorkey = image.get_at((0,0))
+        image.set_colorkey(colorkey, RLEACCEL)
+    return image, image.get_rect()
+
+class NumberPNG(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image, self.rect = load_image('real_zero', -1)
+    def draw(self):
 
 class BubbleNode():
     def __init__(self, width, height, screen):
         self.x = width/2
         self.y = height/3
+        self.y_speed = 5
         self.screen = screen
+        self._width = width
+        self._height = height
     def update(self):
-        pass
+        if self.y >= self._height or self.y <= 0:
+            self.y_speed = -self.y_speed
+        self.y = self.y + self.y_speed
     def draw(self):
         BLACK = (0, 0, 0)
         pygame.draw.circle(self.screen, BLACK, (self.x, self.y), 50, 1)
@@ -43,6 +74,7 @@ def main():
     pygame.display.flip()
     #OBJECTS GO HERE
     bubble1 = BubbleNode(width, height, screen)
+    #Create numbers png
 
     #create game clock
     clock = pygame.time.Clock()
@@ -52,11 +84,12 @@ def main():
         #increment clock
         clock.tick(60)
         #update screen
-        bubble1.update()
+        #bubble1.update()
         #Check if collision will occur
         #updates screen image (redraws screen everytime)
         screen.blit(background, (0, 0))
         bubble1.draw()
+        number0.draw()
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
