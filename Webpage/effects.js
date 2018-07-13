a287c6b5f5c5bc5dc649be711f599e0d8ceecd84
Thomas <thomasdriscoll98@gmail.com>
2018-07-12 20:27:58 -0500

    it's been a while lol

--- Webpage/effects.js
+++ Webpage/effects.js
@@ -1,12 +1,29 @@
 $(document).ready(function(){
   $('.menu_bar_item').mouseenter(function() {
-    $(this).css({
-      backgroundColor: 'Blue'
-    });
+    if($(this).hasClass('current')){
+      $(this).css({
+        'backgroundColor': 'Blue',
+      });
+    }
+    else{
+      $(this).css({
+        'backgroundColor': 'Blue',
+        'border-bottom': '5px solid blue'
+      });
+    }
   }).mouseleave(function(){
-    $(this).css({
-      color: 'LightSkyBlue',
-      backgroundColor: 'DarkBlue'
-    })
+    if($(this).hasClass('current')){
+      $(this).css({
+        'backgroundColor': 'DarkBlue',
+      });
+    }
+    else{
+      $(this).css({
+        'backgroundColor': 'DarkBlue',
+        'border-bottom': '5px solid DarkBlue'
+      });
+    }
+
   });
 });
+// Try to cut this down to like 5 lines, this if-else nonsense hurts my sensibilities
